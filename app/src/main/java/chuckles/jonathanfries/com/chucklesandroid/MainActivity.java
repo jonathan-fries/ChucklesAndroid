dccbd6843ae07481704cc84aed712621f337a480
jfries <jonathan.fries@gmail.com>
2018-06-02 23:34:04 -0600

    Project Structure
    Creating project structure, building image files, laying out main chat screen.

---
+++ app/src/main/java/chuckles/jonathanfries/com/chucklesandroid/MainActivity.java
@@ -0,0 +1,41 @@
+package chuckles.jonathanfries.com.chucklesandroid;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText("Cake is not a bear.");
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText("Crumptastic old bears have never been cake or cakey.");
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.mainChatView);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
