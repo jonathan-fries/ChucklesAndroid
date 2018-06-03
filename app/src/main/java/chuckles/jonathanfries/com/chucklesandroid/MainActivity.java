0e12f40ef23f4a50496f1d716d0d2ebaa0883a57
jfries <jonathan.fries@gmail.com>
2018-06-02 23:51:00 -0600

    Committing
    Updated gitignore, added http code in broken state.

--- app/src/main/java/chuckles/jonathanfries/com/chucklesandroid/MainActivity.java
+++ app/src/main/java/chuckles/jonathanfries/com/chucklesandroid/MainActivity.java
@@ -1,15 +1,33 @@
 package chuckles.jonathanfries.com.chucklesandroid;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+
+    // we"ll make HTTP request to this URL to retrieve weather conditions
+    String chatUrl = "https://chucklesthedirtchipmuffin.com/GetRandomChat";
+    //the loading Dialog
+    ProgressDialog pDialog;
+
+    // JSON object that contains the chats yo!
+    JSONObject jsonObj;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -38,4 +56,105 @@ public class MainActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    protected void callWebService() {
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Please wait while retrieving the weather condition ...");
+        pDialog.setCancelable(false);
+
+        // Check if Internet is working
+        if (!isNetworkAvailable(this)) {
+            // Show a message to the user to check his Internet
+            Toast.makeText(this, "Please check your Internet connection", Toast.LENGTH_LONG).show();
+        } else {
+
+            pDialog.show();
+
+            // make HTTP request to retrieve the weather
+            JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                    chatUrl, null, new Response.Listener<JSONObject>() {
+
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        // Parsing json object response
+                        // response will be a json object
+
+
+                        jsonObj = (JSONObject) response.getJSONArray("weather").get(0);
+                        // display weather description into the "description textview"
+                        description.setText(jsonObj.getString("description"));
+                        // display the temperature
+                        temperature.setText(response.getJSONObject("main").getString("temp") + " °C");
+
+                        String backgroundImage = "";
+
+                        //choose the image to set as background according to weather condition
+                        if (jsonObj.getString("main").equals("Clouds")) {
+                            backgroundImage = "https://marwendoukh.files.wordpress.com/2017/01/clouds-wallpaper2.jpg";
+                        } else if (jsonObj.getString("main").equals("Rain")) {
+                            backgroundImage = "https://marwendoukh.files.wordpress.com/2017/01/rainy-wallpaper1.jpg";
+                        } else if (jsonObj.getString("main").equals("Snow")) {
+                            backgroundImage = "https://marwendoukh.files.wordpress.com/2017/01/snow-wallpaper1.jpg";
+                        }
+
+                        // load image from link and display it on background
+                        // We'll use the Glide library
+                        Glide
+                                .with(getApplicationContext())
+                                .load(backgroundImage)
+                                .centerCrop()
+                                .crossFade()
+                                .listener(new RequestListener<String, GlideDrawable>() {
+                                    @Override
+                                    public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                                        System.out.println(e.toString());
+                                        return false;
+                                    }
+
+                                    @Override
+                                    public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                                        return false;
+                                    }
+                                })
+                                .into(weatherBackground);
+
+                        // hide the loading Dialog
+                        pDialog.dismiss();
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Toast.makeText(getApplicationContext(), "Error , try again ! ", Toast.LENGTH_LONG).show();
+                        pDialog.dismiss();
+
+                    }
+
+
+                }
+
+
+            }, new Response.ErrorListener() {
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    VolleyLog.d("tag", "Error: " + error.getMessage());
+                    Toast.makeText(getApplicationContext(), "Error while loading ... ", Toast.LENGTH_SHORT).show();
+                    // hide the progress dialog
+                    pDialog.dismiss();
+                }
+            });
+
+            // Adding request to request queue
+            AppController.getInstance(this).addToRequestQueue(jsonObjReq);
+        }
+
+        ////////////////////check internet connection
+
+    public boolean isNetworkAvailable(final Context context) {
+        final ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+    }
+
 }
+}
+
